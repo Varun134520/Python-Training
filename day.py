f97d5cf0ead0fdc78ba2570b620af55797b4da3b
Varun134520 <varunmali1345@gmail.com>
2025-02-23 12:10:02 +0530

    Operators Examole Are Added

--- day.py
+++ day.py
@@ -1,4 +1,166 @@
-print("this is my frist code in python language")
+'''print("this is my frist code in python language")
 a=10
 b=20
-print(a+b)
+print(a+b)'''
+
+
+
+'''a=10
+
+v=10.3
+
+b="Hello"
+
+print(type(b))'''
+
+
+#taking input from user
+
+'''num= int(input("Enter first number"))
+
+num2= int(input("Enter second Number"))
+
+
+add= num+num2
+
+print(add)'''
+
+
+
+'''#taking information from user 
+
+user_name= input("enter you name")
+
+user_middlename=input ("enter you middle name")
+
+user_surname=input ("enter your surname")
+
+user_Mobileno = ("enter you Mobile number")'''
+
+
+
+
+'''# taking input from user addition 
+
+num=int(input("Enter first number "))
+
+num2=int(input("Enter second number "))
+
+add=num + num2
+ 
+print(add)'''
+
+
+
+'''#taking input from user Substraction
+
+num=int(input("Enter first number "))
+
+num2=int(input("Enter second number "))
+
+add=num - num2
+ 
+print(add)'''
+
+
+
+'''#taking input from user multiplication
+
+num=int(input("Enter first number "))
+
+num2=int(input("Enter second number "))
+
+add=num * num2
+ 
+print(add)'''
+
+
+
+'''#taking input from user division
+
+num=int(input("Enter first number "))
+
+num2=int(input("Enter second number "))
+
+add=num / num2
+ 
+print(add)'''
+
+
+'''#taking the input from user for Greater than Value
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("===Greater than ===",A>B)'''
+
+'''#taking the input from user for Smaller than Value
+
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("====Smaller than ====",A<B)'''
+
+
+'''#taking the input from user for Greater than of equal too Value
+
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("====Greater than equal to ====",A>=B)
+'''
+
+'''#taking the input from user for smaller than of equal too Value
+
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("====smaller than equal to ====",A<=B)'''
+
+'''#taking the input from user for not equal too Value
+
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("====not  equal to ====",A!=B)'''
+
+
+
+'''#taking the input from user for equal too Value
+
+A=int(input("Enter the first choice "))
+
+B=int(input("Enter Second choice "))
+
+
+print("==== equal to ====",A==B)'''
+
+
+
+
+
+'''#identity operator  IS
+
+Ab=2020
+
+bc=2020
+
+print("This is Logic of IS ",Ab is bc) '''
+
+
+#identity operator  IS NOT
+
+Ab=2022
+
+bc=2020
+
+print("This is Logic of IS NOT ",Ab is not  bc)
